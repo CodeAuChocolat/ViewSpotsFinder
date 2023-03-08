13c60353168b931d53102ba0800e9b776b8ac007
Max Kost <m.kost@ixperienz.com>
2023-03-08 12:48:50 +0100

    Add benchmarks

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.IO;
+using System.Text.Json;
+using ViewSpots;
+using ViewSpots.Models;
+
+namespace Benchmarks
+{
+  public class Program
+  {
+    public class ViewSpotFinderBenchmarks
+    {
+      private readonly Mesh _mesh;
+
+      public ViewSpotFinderBenchmarks()
+      {
+        using var stream = new FileStream(@"C:\Temp\mesh\mesh_x_sin_cos_10000.json", FileMode.Open);
+        _mesh = JsonSerializer.Deserialize<Mesh>(stream)!;
+      }
+
+      [Benchmark]
+      public List<ElementValue> DefaultImplementation10000()
+      {
+        var finder = new ViewSpotFinder();
+        return finder.Execute(_mesh).ToList();
+      }
+
+      [Benchmark]
+      public List<ElementValue> TrivialImplementation10000()
+      {
+        var finder = new TrivialViewSpotFinder();
+        return finder.Execute(_mesh).ToList();
+      }
+    }
+
+    public static void Main(string[] args)
+    {
+      var summary = BenchmarkRunner.Run<ViewSpotFinderBenchmarks>();
+    }
+  }
+}
